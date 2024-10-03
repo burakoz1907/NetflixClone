233232ef16ded07e1c0212628c034a74c3db09bf
Burak <ozb5076@gmail.com>
2024-10-03 03:26:23 +0300

    env localde keyler degiştirildi

--- app/components/carousel.jsx
+++ app/components/carousel.jsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import React, { useEffect } from "react";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { useRouter } from "next/navigation";
-
+import Image from 'next/image'; // next/image bileşenini içe aktar
 
 const Carousel = ({ movies = [] }) => {
   const [sliderRef, sliderInstanceRef] = useKeenSlider({
@@ -25,17 +25,20 @@ const Carousel = ({ movies = [] }) => {
     if (sliderInstanceRef.current) {
       sliderInstanceRef.current.update();
     }
-  }, [movies]);
+  }, [movies, sliderInstanceRef]); // sliderInstanceRef'i ekledik
 
   return (
     <div ref={sliderRef} className="keen-slider">
       {movies.length > 0 ? (
         movies.map((movie) => (
           <div key={movie.id} className="keen-slider__slide" onClick={() => handleMovieClick(movie.id)}>
-            <img
+            <Image
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
               alt={movie.title}
-              style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+              layout="responsive" // Resmi orantılı olarak boyutlandırır
+              width={500} // Genişlik
+              height={750} // Yükseklik
+              style={{ objectFit: 'cover' }}
             />
           </div>
         ))
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Homepage from "./components/homepage/homepage";
 
-
 export default function Home() {
   return (
     <div> 
